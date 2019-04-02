b48b81e0d6e3d3ae0ae5c20d91107ba9fed0c67a
geraltofrivia99 <geralt.from.rivi9@yandex.ru>
2019-04-02 16:55:44 +0700

    sec

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import startLogo from './assets/start.png';
 import cn from 'classnames';
 
-import { blocksArray } from './const';
+import { blocksArray, recentlyArray, mostReadArray } from './const';
 
 import './App.scss';
 
@@ -11,6 +11,19 @@ const SmallBox = React.memo(({ img, text }) => (
   <p className="small-block">
     <img src={img} alt={text} />
   </p>
+));
+
+const Row = React.memo(({ img, text }) => (
+  <div className="menu-row">
+    <img src={img} alt={text} />
+    {text}
+  </div>
+));
+
+const bigBlock = React.memo(({ img, text }) => (
+  <div className="bigBlock">
+
+  </div>
 ));
 
 const App = () => {
@@ -27,6 +40,15 @@ const App = () => {
       <div className={startClasNames}>
         <div className="menu-left-side">
           {blocksArray.map((block, index) => <SmallBox img={block.img} text={block.name} key={block.name + index} />)}
+        </div>
+        <div className="menu-list">
+          <h3 className="menu-list-title">Recently</h3>
+          {recentlyArray.map((cur, index) => <Row text={cur.name} img={cur.img} key={cur.name + index}/>)}
+          <h3 className="menu-list-title">Most Read</h3>
+          {mostReadArray.map((cur, index) => <Row text={cur.name} img={cur.img} key={cur.name + index}/>)}
+        </div>
+        <div className="grid-side">
+
         </div>
       </div>
     </section>
--- src/const.js
+++ src/const.js
@@ -2,6 +2,7 @@ import edit from './assets/edit.svg';
 import picture from './assets/picture.svg';
 import off from './assets/power-button-off.svg';
 import settings from './assets/settings.svg';
+import folder from './assets/folder.svg';
 
 export const blocksArray = [
   {
@@ -16,4 +17,74 @@ export const blocksArray = [
   {
     name: 'off', img: off
   }
+];
+
+export const recentlyArray = [
+  {
+    name: 'Paint', img: folder
+  },
+  {
+    name: 'Main Activity', img: folder
+  },
+  {
+    name: 'New Folder', img: folder
+  }
+];
+
+export const mostReadArray = [
+  {
+    name: 'Blabla', img: folder
+  },
+  {
+    name: 'Somthing New', img: folder
+  },
+  {
+    name: 'Very new folder', img: folder
+  },
+  {
+    name: 'Blabla', img: folder
+  },
+  {
+    name: 'Somthing New', img: folder
+  },
+  {
+    name: 'Very new folder', img: folder
+  },
+  {
+    name: 'Blabla', img: folder
+  },
+  {
+    name: 'Somthing New', img: folder
+  },
+  {
+    name: 'Very new folder', img: folder
+  },
+  {
+    name: 'Blabla', img: folder
+  },
+  {
+    name: 'Somthing New', img: folder
+  },
+  {
+    name: 'Very new folder', img: folder
+  },
+  {
+    name: 'Blabla', img: folder
+  },
+  {
+    name: 'Somthing New', img: folder
+  },
+  {
+    name: 'Very new folder', img: folder
+  },
+  {
+    name: 'Blabla', img: folder
+  },
+  {
+    name: 'Somthing New', img: folder
+  },
+  {
+    name: 'Very new folder', img: folder
+  },
+  
 ]
